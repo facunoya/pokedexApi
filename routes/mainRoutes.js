17a64533452c284e2e1b86777531d276669833bc
Facundo Noya <facunoya@hotmail.com>
2023-07-21 21:47:00 -0300

    acomodando ruta de inicio

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mainControllers = require('../controllers/mainControllers');
 
-router.get('/', mainControllers.getHome)
+router.get('/bus', mainControllers.getHome)
 router.get('/pokemon', mainControllers.getPokemon)
 router.get('/buscar', mainControllers.buscarUno)
-router.get('/buscarTodos', mainControllers.buscarTodos)
+router.get('/', mainControllers.buscarTodos)
 router.get('/todos2', mainControllers.todos2)
 
 module.exports = router
